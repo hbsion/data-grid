a4a020c435467e555e86d7ea752a9af7b7cf9237
Pauli B <pauli.buccini@gmail.com>
2016-04-26 18:50:12 +0700

    sort works

--- imports/samples/MyDataGrid/MyDataGrid.js
+++ imports/samples/MyDataGrid/MyDataGrid.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import DataGrid from 'react-datagrid';
+import sorty from 'sorty';
 import 'react-datagrid/index.css';
 
 // const data = [
@@ -16,30 +17,84 @@ const columns = [
   { name: 'email'},
 ];
 
+let SORT_INFO = [ { name: 'firstName', dir: 'asc'}];
+
+// function sort(arr) {
+//   return sorty(SORT_INFO, arr);
+// }
+
+class MyDataGrid extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      sortInfo: [ { name: 'firstName', dir: 'asc'}],
+    };
+
+    this.handleColumnOrderChange = this.handleColumnOrderChange.bind(this);
+    this.handleSortChange = this.handleSortChange.bind(this);
+  }
+  render() {
+    const {
+      sortInfo,
+    } = this.state;
+
+    let list = this.props.list;
+    list = [].concat(list);
+    list = sorty(sortInfo, list);
+
+    return <DataGrid
+      idProperty='id'
+      dataSource={list}
+      columns={columns}
+      style={{height: 500}}
+      sortInfo={sortInfo}
+      onSortChange={this.handleSortChange}
+      onColumnOrderChange={this.handleColumnOrderChange}
+    />;
+  }
+
+  handleColumnOrderChange(index, dropIndex) {
+    const col = columns[index];
+    columns.splice(index, 1);  // delete from index, 1 item
+    columns.splice(dropIndex, 0, col);
+    this.setState({});
+  }
+  handleSortChange(sortInfo) {
+    // console.log(sortInfo)
+
+
+    // console.log('💣', data);
+    // console.log('🔒', data2);
+    this.setState({sortInfo});
+  }
+}
+
+
+// const MyDataGrid = (props) => {
+//   return (
+//     <DataGrid idProperty="id" dataSource={props.list} columns={columns} />
+//   );
+// };
+
+export default MyDataGrid;
+
 
 // const MyDataGrid = React.createClass({
-//   handleColumnOrderChange: function (index, dropIndex){
-//     var col = columns[index]
-//     columns.splice(index, 1) //delete from index, 1 item
-//     columns.splice(dropIndex, 0, col)
-//     this.setState({})
+//   handleColumnOrderChange: function(index, dropIndex) {
+//     const col = columns[index];
+//     columns.splice(index, 1); // delete from index, 1 item
+//     columns.splice(dropIndex, 0, col);
+//     this.setState({});
 //   },
-//   render: function(){
+//   render: function() {
 //     return <DataGrid
 //       idProperty='id'
-//       dataSource={data}
+//       dataSource={this.props.list}
 //       columns={columns}
 //       style={{height: 500}}
 //       onColumnOrderChange={this.handleColumnOrderChange}
-//     />
-//   }
-// })
-
-
-const MyDataGrid = (props) => {
-  return (
-    <DataGrid idProperty="id" dataSource={props.list} columns={columns} />
-  );
-};
-
-export default MyDataGrid;
+//     />;
+//   },
+// });
