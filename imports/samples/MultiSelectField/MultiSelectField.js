8dc9209282dde82b4c73cc833eeda992a645fb78
Pauli B <pauli.buccini@gmail.com>
2016-04-27 18:32:17 +0700

    preparing multi-select

--- imports/samples/MultiSelectField/MultiSelectField.js
+++ imports/samples/MultiSelectField/MultiSelectField.js
@@ -11,10 +11,6 @@ const FLAVOURS = [
 	{ label: 'Peppermint', value: 'peppermint' },
 ];
 
-const WHY_WOULD_YOU = [
-	{ label: 'Chocolate (are you crazy?)', value: 'chocolate', disabled: true },
-].concat(FLAVOURS.slice(1));
-
 const MultiSelectField = React.createClass({
   displayName: 'MultiSelectField',
   propTypes: {
@@ -32,32 +28,11 @@ const MultiSelectField = React.createClass({
     console.log('You\'ve selected:', value);
     this.setState({ value });
   },
-  toggleDisabled(e) {
-    this.setState({ disabled: e.target.checked });
-  },
-  toggleChocolate(e) {
-    const crazy = e.target.checked;
-    this.setState({
-      crazy: crazy,
-      options: crazy ? WHY_WOULD_YOU : FLAVOURS,
-    });
-  },
   render() {
     return (
       <div className="section">
         <h3 className="section-heading">{this.props.label}</h3>
-        <Select multi simpleValue disabled={this.state.disabled} value={this.state.value} placeholder="Select your favourite(s)" options={this.state.options} onChange={this.handleSelectChange} />
-
-        <div className="checkbox-list">
-          <label className="checkbox">
-            <input type="checkbox" className="checkbox-control" checked={this.state.disabled} onChange={this.toggleDisabled} />
-            <span className="checkbox-label">Disable the control</span>
-          </label>
-          <label className="checkbox">
-            <input type="checkbox" className="checkbox-control" checked={this.state.crazy} onChange={this.toggleChocolate} />
-            <span className="checkbox-label">I don't like Chocolate (disabled the option)</span>
-          </label>
-        </div>
+        <Select multi simpleValue disabled={this.state.disabled} value={this.state.value} placeholder="Select groups" options={this.state.options} onChange={this.handleSelectChange} />
       </div>
     );
   },
