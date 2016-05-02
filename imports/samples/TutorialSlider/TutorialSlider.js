a2b095ef51dfd2094b24346afc99f43b7c29d678
Lauricio Su <me@lauricio.com>
2016-05-02 16:09:07 +0700

    wip

--- imports/samples/TutorialSlider/TutorialSlider.js
+++ imports/samples/TutorialSlider/TutorialSlider.js
@@ -35,40 +35,50 @@ const carouselStyle = {
   },
 };
 
-
-class Carousel extends React.Component {
-  render() {
-    return (
-      <ReactSwipe className="carousel" swipeOptions={{continuous: false}} style={carouselStyle}>
-          <div className="tutorial-panel color:warning">PANE 1</div>
-          <div className="tutorial-panel color:action">PANE 2</div>
-          <div className="tutorial-panel color:critical">PANE 3</div>
-      </ReactSwipe>
-    );
-  }
-}
-
-const style = {
+const buttonsStyle = {
   button: {
-    width: '100%',
+    flex: '1',
+    // width: '100%',
     height: '100%',
   },
 };
 
+class Carousel extends React.Component {
+  next() {
+    console.log('🍋', this);
+    this.refs.reactSwipe.next();
+  }
+  prev() {
+    this.refs.reactSwipe.prev();
+  }
+  render() {
+    console.log('👻', this);
+    return (
+      <oo-box class="flex:column h:100p">
+        <oo-item class="flex:1">
+          <div className="oo-absolute">
+            <Device>
+              <ReactSwipe ref="reactSwipe" className="carousel" swipeOptions={{continuous: false}} style={carouselStyle}>
+                  <div className="tutorial-panel color:warning">PANE 1</div>
+                  <div className="tutorial-panel color:action">PANE 2</div>
+                  <div className="tutorial-panel color:critical">PANE 3</div>
+              </ReactSwipe>
+            </Device>
+          </div>
+        </oo-item>
+        <oo-box class="flex:50px">
+          {/* <Button primary className="" style={buttonsStyle.button} onClick={this.prev.bind(this)}>Back</Button> */}
+          <Button primary className="" style={buttonsStyle.button} onClick={this.next.bind(this)}>Next</Button>
+        </oo-box>
+      </oo-box>
+    );
+  }
+}
+
+
 const TutorialSlider = (props) => {
   return (
-    <oo-box class="flex:column h:100p">
-      <oo-item class="flex:1">
-        <div className="oo-absolute">
-          <Device>
-            <Carousel/>
-          </Device>
-        </div>
-      </oo-item>
-      <oo-item class="flex:50px">
-        <Button primary className="" style={style.button}>Next</Button>
-      </oo-item>
-    </oo-box>
+    <Carousel/>
   );
 };
 
